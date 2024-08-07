9c8f3038a43f725dba7423e048809501afa614b0
Sakthiyanathan25 <selvamnato.biotech25@gmail.com>
2024-08-08 01:13:37 +0530

    Complete frontend #5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import envCompatible from 'vite-plugin-env-compatible';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()
-    ,
-    envCompatible(),
-    
-  ]
-  
-})
+  plugins: [react(),envCompatible(),],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        // Add other entry points if necessary
+      }
+    }
+  },
+  // Configure other options as necessary
+});
